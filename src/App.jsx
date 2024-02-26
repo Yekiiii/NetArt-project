7f02ea25bb39194cd82037bbbaf0bc279c47b7b7
Yekiiii <kaartikey20@gmail.com>
2024-02-27 05:28:27 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='header'>
+      <img className="logo-img" src="public\logo.png" alt="" />
+      
+     </div>
+
+     <div className='flex-container-1'>
+      <img className="trophy-img" src="public\1.png" alt="" />
+      <div>
+        <p> <b>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</b></p>
+        <ul>
+          <li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+          <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
+        </ul>
+        <img className="award-img" src="public\2.png" alt="" />
+        <p>Government of India has awarded the <b>"National Energy Conservation Award 2018"</b>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
+      </div>
+      </div>
+    <div className='flex-container-2'>
+
+      <p className='center-text'>
+      INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.
+      </p>
+
+      <img className='th-image' src="public\3.png" alt="" />
+      <p className='center-text'>
+      Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors
+      </p>
+      
+
+    </div>
+    <hr />
+    <div>
+
+    
+    <p className='center-text'><b>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</b></p>
+
+    <p className='center-text'>CHEMICALS & PROCESS <span className='red'><b>|</b></span> POWER <span className='red'><b>|</b></span> WATER & WASTE WATER <span className='red'><b>|</b></span> OILS & GAS <span className='red'><b>|</b></span> PHARMA <span className='red'><b>|</b></span> SUGARS & DISTILLERIES <span className='red'><b>|</b></span> PAPER & PULP <span className='red'><b>|</b></span> MARINE & DEFENCE <span className='red'><b>|</b></span> METAL & MINING <span className='red'><b>|</b></span> FOOD & BEVERAGE <span className='red'><b>|</b></span> PETROCHEMICAL & REFINERIES <span className='red'><b>|</b></span> SOLAR <span className='red'><b>|</b></span> BUILDING <span className='red'><b>|</b></span> HVAC <span className='red'><b>|</b></span> FIRE FIGHTING <span className='red'><b>|</b></span> AGRICULTURE & RESIDENTIAL</p>
+
+
+
+    <div className='footer'>
+
+      <div>
+      
+        <p className='white footer-component'><i class="fa-solid fa-phone"></i> Toll free <b>1800 200 1234</b></p>
+      </div>
+
+      <div>
+
+      
+        <p className='white footer-component'><i class="fa-brands fa-facebook"></i> www.facebook.com/cripumps</p>
+      </div>
+      <div>
+      
+        <p className='white footer-component'><i class="fa-solid fa-globe"></i> www.crigroups.com</p>
+      </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
