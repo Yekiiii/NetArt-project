ea89d15cff17b749892fb8545398776a44ef719f
Yekiiii <kaartikey20@gmail.com>
2024-02-27 05:33:28 +0530

    images fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,19 @@ function App() {
   return (
     <>
      <div className='header'>
-      <img className="logo-img" src="public\logo.png" alt="" />
+      <img className="logo-img" src="logo.png" alt="" />
       
      </div>
 
      <div className='flex-container-1'>
-      <img className="trophy-img" src="public\1.png" alt="" />
+      <img className="trophy-img" src="1.png" alt="" />
       <div>
         <p> <b>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</b></p>
         <ul>
           <li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
           <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
         </ul>
-        <img className="award-img" src="public\2.png" alt="" />
+        <img className="award-img" src="2.png" alt="" />
         <p>Government of India has awarded the <b>"National Energy Conservation Award 2018"</b>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</p>
       </div>
       </div>
@@ -28,7 +28,7 @@ function App() {
       INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.
       </p>
 
-      <img className='th-image' src="public\3.png" alt="" />
+      <img className='th-image' src="3.png" alt="" />
       <p className='center-text'>
       Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors
       </p>
